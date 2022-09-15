977989eec69f3c02852144deb9cd0377aa28b2d6
Helix088 <pat20001@byui.edu>
2022-09-14 21:47:17 -0600

    updated portfolio with week 01

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-let orderedList = document.getElementById("ol");
-
 const links = [
-    {
-        label: "Week1 notes",
-        url: "week1/index.html"
-    }
-]
+  {
+    label: "Week 01",
+    url: "/week01/index.html",
+  },
+];
+
+let orderedList = document.getElementById("tobLinks");
+
+links.forEach(item => {
+    let li = document.createElement("li");
+    orderedList.appendChild(li);
+    let a = document.createElement("a");
+    a.textContent = item.label;
+    a.href = item.url;
+    li.appendChild(a);
+});
