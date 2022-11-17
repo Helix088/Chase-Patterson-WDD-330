0c6c31174b0b56fdd758d11616c8c9336177edb9
Chase Patterson <chase.patterson@uvu.edu>
2022-11-17 14:47:14 -0700

    added quakes

--- quakes/js/utilities.js
+++ quakes/js/utilities.js
@@ -1,3 +1,21 @@
-function getJSON(url) {
-    fetch(url).then((response) => response.json()).then((data) => console.log(data));
+const url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02";
+
+export function getJSON(url) {
+  fetch(url)
+    .then(function (response) {
+        if(!response.ok) {
+            throw Error(response.statusText);
+        } else {
+            response.json();
+        }
+    }).catch(function(error) {
+        console.log(error);
+    });
 }
+
+export const getLocation = function(options) {
+    return new Promise(function(resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    })
+}
+
