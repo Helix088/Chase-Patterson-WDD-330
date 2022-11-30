9aa4a87ffb372d53f82e1980d7fd376c24853d70
Helix088 <pat20001@byui.edu>
2022-11-29 22:32:02 -0700

    Have to find new api, the one I was using got deleted

--- quakes/js/index.js
+++ quakes/js/index.js
@@ -3,13 +3,30 @@ import { getJSON, getLocation } from "./utilities.js";
 const baseUrl =
   "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02";
 
-function testGetQuakesForLocation() {
+async function testGetQuakesForLocation() {
   // call the getLocation function to get the lat/long
-  let location = getLocation();
+  const location = await getLocation();
+  debugger;
+  console.log(location);
+
   // use that information to build out the correct URL
-  const geoUrl = baseUrl + location; // add location information here
+  const geoUrl =
+    baseUrl +
+    `&latitude=${location.coords.latitude}&longitude=${location.coords.longitude}&maxradiuskm=100`; // add location information here
   // use the url to request the correct quakes
-    return geoUrl;
+  const response = await getJSON(geoUrl);
+  debugger;
+  console.log(response);
+  return {
+    response,
+    location,
+  };
   //log out the quakes for now.
 }
-getQuakesForLocation();
+
+testGetQuakesForLocation()
+  .then((res) => {
+    debugger;
+    console.log(res);
+  })
+  .catch(console.log);
